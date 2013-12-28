82a53626c7f90afd23dc93a7eaac6fecbca7b9fc
BambangYM <family.manullang@gmail.com>
2013-12-28 22:32:21 +0700

    Theme Front (Basic)

---
+++ themes/views/layouts/Header.php
@@ -0,0 +1,33 @@
+<header>
+<div id="logo"></div>
+<div id="panel-above">
+<input type="search" value="Cari Tempat" id="form_cari" title="Temukan Lokasi tujuan anda di sini">
+<button id="tombol_cari" type="button" >Cari</button>
+
+<div id="loginContainer">
+                <a href="#" id="loginButton"><span>Login</span><em></em></a>
+                <div style="clear:both"></div>
+                <div id="loginBox">                
+                    <form id="loginForm">
+                        <fieldset id="body">
+                            <fieldset>
+                                <label for="email">Email Address</label>
+                                <input type="text" name="email" id="email" />
+                            </fieldset>
+                            <fieldset>
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password" />
+                            </fieldset>
+                            <h3 style="text-align:center; color:#39F; margin-top:0px; padding-top:0px"> Login With:</h3>
+                            <input type="submit" id="login" value="Sign in" />
+                            <img src="../../image/facebook.png" style="float:right; margin-right:10px">
+                            <img src="../../image/twitter.png" style="float:right; margin-right:10px;">
+                            <a href="register.html"><input type="button" id="register" value="Register"/></a>
+                            <label for="checkbox"><input type="checkbox" id="checkbox" />Remember me</label>
+                        </fieldset>
+                        <span><a href="#">Forgot your password?</a></span>
+                    </form>
+                </div></div>
+
+</div>
+</header>
